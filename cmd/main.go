a00d3c23f595f537de4d5d099756bb685b17ddcc
Fae LeDawson <fae@ledawson.com>
2025-06-13 13:59:05 -0700

    Add module and go generate support

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,1 @@
-//go:generate docker run --rm -v ${PWD}:/local openapitools/openapi-generator-cli generate -i /api/openapi.yaml -g go -o api/ --package-name api
+//go:generate docker run --rm -v ${pwd}:/local openapitools/openapi-generator-cli generate -i /local/api/openapi.yaml -g go -o api/ --package-name faelist.com/sorterv1
---
+++ go.mod
@@ -0,0 +1,3 @@
+module faelist.com/sorterv1
+
+go 1.24.4
